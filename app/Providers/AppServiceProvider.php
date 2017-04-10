b44e84f790820db31e1a93276f8c450bc58b4517
Antony Sklyar <antony.sklyar@gmail.com>
2017-04-10 23:16:09 +0300

    Upgrade to the latest Laravel and update all of the dependencies (both composer and npm).

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Laravel 5.4 introduced emoji support and uses utf8mb4 encoding, thus on old MySQL versions it needs
+        // adjustment, as said here https://laravel-news.com/laravel-5-4-key-too-long-error
+        Schema::defaultStringLength(191);
+
+        // variable view_name can be used in any Blade view to retrieve its own name
         view()->composer('*', function ($view) {
             $view_name = str_replace('.', '-', $view->getName());
             view()->share('view_name', $view_name);
